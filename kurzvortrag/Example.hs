a29453a73e6bdaead4762ff39b4cb041e1d83e8e
David Kraeutmann <kane@kane.cx>
2015-04-13 11:30:21 +0200

    Change types in Example.hs to be a bit simpler.

--- kurzvortrag/Example.hs
+++ kurzvortrag/Example.hs
@@ -1,7 +1,7 @@
-speed :: Event Float
+speed :: GameWire a Float
 speed  =  pure (-1) . moving Left
       <|> pure ( 1) . moving Right
       <|> pure ( 0)
               
-pos :: Signal Float
+pos :: GameWire a Float
 pos = integral 0 . speed
