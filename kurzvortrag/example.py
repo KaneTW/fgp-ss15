1a8a3049060f09341f806d31d9498dd390302979
David Kraeutmann <kane@kane.cx>
2015-04-13 11:34:13 +0200

    dir is highlighted uglily in example.py

--- kurzvortrag/example.py
+++ kurzvortrag/example.py
@@ -1,9 +1,9 @@
 def speed(val):
-    dir = val.get_value()
+    d = val.get_value()
     delta = val.get_interval_in_milliseconds()/1000.0
-    if dir == Direction.LEFT:
+    if d == Direction.LEFT:
         return -delta
-    elif dir == Direction.RIGHT:
+    elif d == Direction.RIGHT:
         return delta
     return 0
     
